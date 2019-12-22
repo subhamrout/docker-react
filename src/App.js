436ce2249a6373225f77ff08bd23ada891781fb9
subhamrout <subhamrout39@gmail.com>
2019-12-22 18:54:34 +0530

    changes made in app.js for text output

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is Subham
+         Hello,This is Subham
         </p>
         <a
           className="App-link"
